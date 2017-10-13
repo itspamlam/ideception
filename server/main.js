bae73d49897049503e856c18efca0efe6bb5e36f
Samantha Salley <samanthasalley@gmail.com>
2017-10-12 21:42:53 -0700

    add boilerplate server code

---
+++ server/main.js
@@ -0,0 +1,32 @@
+const bodyParser    = require('body-parser'),
+express       = require('express'),
+path          = require('path'),
+app           = express();
+
+/**
+* Create middleware function for scraper controller to add locals object to request for adding data
+*/
+const createLocalsObj = (req, res, next) => {
+req.locals = {};
+next();
+};
+
+/**
+* Express middleware to serve all static files from client folder
+*/
+app.use(express.static(path.join(__dirname, 'client')));
+
+
+/**
+* Body-parser middleware to automatically parse request body data and store in req.body
+*/
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+/**
+* Establish db connection
+*/
+const port = 8080;
+app.listen(port);
+console.log(`Server running on port: ${port}`);
