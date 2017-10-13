a312d1deb17ebf34c2b9e3cfc07e52493c7a4a01
Samantha Salley <samanthasalley@gmail.com>
2017-10-13 15:33:05 -0700

    add faker package and create faker controller for generating random words and frequencies

--- server/main.js
+++ server/main.js
@@ -6,6 +6,7 @@ const bodyParser    = require('body-parser'),
 /**
  * require controllers
  */
+const fakerController = require('./controllers/fakerController.js');
 const scraperController = require('./controllers/scraperController.js');
 const frequencyController = require('./controllers/frequencyController.js');
 /**
@@ -15,6 +16,24 @@ const createLocalsObj = (req, res, next) => {
   req.locals = {};
   next();
 };
+// Add createLocalsObj as middleware
+app.use(createLocalsObj);
+
+/**
+ * allowCrossDomain - Sets headers to allow for CORS
+ * 
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ */
+const allowCrossDomain = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+// Add allowCrossDomain as middleware
+app.use(allowCrossDomain);
 
 /**
  * Express middleware to serve all static files from client folder
@@ -31,9 +50,10 @@ app.use(bodyParser.json());
 /**
  * Establish route to request scraped data
  */
-app.get('/scraper', createLocalsObj, scraperController.getMediumData, scraperController.getRedditData, frequencyController.sanitizeTitles);
+app.get('/scraper', scraperController.getMediumData, scraperController.getRedditData, frequencyController.sanitizeTitles);
 // Test route for data not scraped
 app.get('/frequency-test', frequencyController.sanitizeTitles);
+app.get('/faker', fakerController.generateRandomWords, fakerController.generateRandomFrequencies);
 
 
 /**
