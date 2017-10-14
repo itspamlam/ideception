5f3cd4eb671f85f925da3e3aff453cc814002757
Samantha Salley <samanthasalley@gmail.com>
2017-10-14 11:37:07 -0700

    modularize routing into separate file

---
+++ server/routes/index.js
@@ -0,0 +1,57 @@
+const express    = require('express'),
+      router     = express.Router();
+
+/**
+ * require controllers
+ */
+const frequencyController = require('../controllers/frequencyController.js'),
+scraperController = require('../controllers/scraperController.js'),
+fakerController = require('../controllers/fakerController.js'),
+ideaController = require('../controllers/ideaController.js'),
+userController = require('../controllers/userController.js');
+
+
+/**
+ * Scraped data handler
+ *  - scrapes data from Medium and Reddit then 
+ *    sanitizes the data before outputting to client
+ */
+router
+  .route('/scraper')
+  .get(scraperController.getMediumData, scraperController.getRedditData, frequencyController.sanitizeTitles);
+
+/**
+ * Random data handler
+ *  - generates random words and random frequencies for those words
+ *    before outputting to client.
+ *  - accepts request input if received
+ */
+router // request random data
+  .route('/faker')
+  .get(fakerController.generateRandomWords, fakerController.generateRandomFrequencies);
+
+/**
+ * Idea handlers
+ *  - Get all ideas
+ *  - Create a new idea
+ *  - Get all ideas for user
+ */
+router
+  .route('/ideas')
+  .get(ideaController.getIdea)
+  .post(ideaController.createIdea);
+router
+  .route('/ideas/:userId')
+  .get(ideaController.getIdeasByUser);
+
+/**
+ * Users handler
+ *  - Get existing user
+ *  - Create a new user
+ */
+router
+  .route('/user')
+  .get(userController.getUser)
+  .post(userController.createUser);
+
+module.exports = router;
