4c276a778fa547a3ac85a7cb96d12e28d1139d6a
freemanchen <freemanxchen@gmail.com>
2017-10-18 11:09:50 -0700

    beautify click history

--- client/src/clickedWords/ClickedWords.js
+++ client/src/clickedWords/ClickedWords.js
@@ -10,18 +10,25 @@ const ClickedWords = (props) => {
 
 //FOR LOOP//
   let words2 = []
-  for (let i = props.clickedWords.length-1; i >= 0; i -= 1){
-    words2.push(<li>{props.clickedWords[i]}</li>);    
+  for (let i = props.clickedWords.length-1; i >= 0; i -= 1) {
+    if (props.clickedWords[i] !== props.targetWord) {
+      words2.push(<li key={i} className={props.clickedWords[i + 1] === props.targetWord ? 'finish-history': ''}>{props.clickedWords[i]}</li>);
+    }
   }
-  
+
   return (
-    <div>
-      <h3> Current Score: </h3>
-      { words2.length }
-      <h3>Target Word: </h3>
-      {props.targetWord}
-      <h3>Clicked Words: </h3>
-       {words2}
+    <div className="history-display">
+      <h3 className="score">
+        <span>Score:</span>
+        { words2.length }
+      </h3>
+
+      <h3 className="target-word">
+        {props.targetWord}
+      </h3>
+      <ul>
+        {words2}
+      </ul>
     </div>
   )
 }
